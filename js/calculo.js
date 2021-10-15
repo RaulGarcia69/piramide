de05ab4a9c68da214f0cb908f4ef444daa6d88e9
raulgarcia69 <raulgarcia0144@gmail.com>
2021-10-15 17:07:50 +0200

    gwsgw

--- js/calculo.js
+++ js/calculo.js
@@ -1,11 +1,16 @@
 function calculo() {
-    var cuenta = document.getElementById("cuenta").value;
-    var piramide = "";
-    var resultado = "";
-    for (var i = 0; i <= cuenta; i++) {
-        piramide = piramide + "#";
-        resultado = resultado + "<p>" + piramide + "</p>";
+    if (document.getElementById("piramide")) {
+        document.getElementById("piramide").remove()
+    } else {
+        var cuenta = document.getElementById("cuenta").value;
+        var piramide = "";
+        var resultado = "";
+        for (var i = 0; i <= cuenta; i++) {
+            piramide = piramide + "#";
+            resultado = resultado + "<p id='piramide'>" + piramide + "</p>";
+        }
+        var form = document.getElementById("formulario");
+        form.innerHTML = form.innerHTML + resultado;
     }
-    var form = document.getElementById("piramide");
-    form.innerHTML = form.innerHTML + resultado;
+
 }
